ad0145b4846deb05c931d479ee73445dbbfad85f
Helmi Touati <touatizh@gmail.com>
2024-03-06 21:09:33 +0100

    feat(currency): Optimize currency converter with Decimal conversion
    - Updated the function to extract numerical values using regex and converting them to `Decimal`.
    - This modification addresses potential issues with conversion results having the currency name attached to them at the end of the string, providing a more reliable tool for converting amounts between different currencies.
    
    The update to the `currency_converter` function marks a significant improvement in the application's multi-currency capabilities, ensuring users benefit from accurate and trustworthy currency conversions.

--- server/core/utils/currency_converter.py
+++ server/core/utils/currency_converter.py
@@ -1,9 +1,12 @@
+import re
 import requests
 import bs4
+from decimal import Decimal
 
 def currency_converter(base, to, amount):
     url = f"https://www.xe.com/currencyconverter/convert/?Amount={amount}&From={base}&To={to}"
     response = requests.get(url)
     soup = bs4.BeautifulSoup(response.text, "html.parser")
     result = soup.find("p", {"class": "result__BigRate-sc-1bsijpp-1 dPdXSB"}).get_text(strip=True)
-    return result
+    num_result = Decimal(re.findall(r"[0-9.]+", result)[0])
+    return num_result
