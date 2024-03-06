dca4b7d5a631f4bdf003abadd2fea40441163fdc
Helmi Touati <touatizh@gmail.com>
2024-03-06 11:47:15 +0100

    feat(transactions): Add positive amount validator
    - Implemented `TransactionAmountValidator` function to ensure transaction amounts are positive.
    - Integrated the validator within the `Transaction` model's `amount` field definition, applying it as a constraint that automatically checks and validates transaction amount values upon creation or update.
    
    This validation strategy prevents the recording of transactions with negative amounts, enhancing user inputs reliability.

--- server/core/transactions/models.py
+++ server/core/transactions/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from core.utils.choices import TransactionType, Currency, Category
 from core.accounts.models import Account
+from core.utils.validators import TransactionAmountValidator
 
 class Transaction(models.Model):
     id = models.AutoField(primary_key=True)
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="transactions")
     type = models.CharField(max_length=16, choices=TransactionType.choices, default=TransactionType.EXP)
     category = models.CharField(max_length=48, choices=Category.choices)
-    amount = models.DecimalField(max_digits=19, decimal_places=3)
+    amount = models.DecimalField(max_digits=19, decimal_places=3, validators=[TransactionAmountValidator])
     currency = models.CharField(max_length=5, choices=Currency.choices)
     date_time = models.DateTimeField()
     description = models.TextField(blank=True, null=True)
--- server/core/utils/validators.py
+++ server/core/utils/validators.py
@@ -4,3 +4,7 @@ from rest_framework.validators import ValidationError
 def HexColorValidator(color):
     if not re.match(r'^#(?:[0-9a-fA-F]{3}){1,2}$', color):
         raise ValidationError("This field must be a hex color code.")
+
+def TransactionAmountValidator(amount):
+    if amount < 0:
+        raise ValidationError("This field must be greater than 0.")
