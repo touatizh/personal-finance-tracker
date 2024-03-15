aae35975a2325c2c4c9ebff9b75aa4320aaaeb9a
Helmi Touati <touatizh@gmail.com>
2024-03-15 08:24:21 +0100

    feat(UI): Implement add account feature
    - Introduced the `AccountForm` component for account creation.
    - Utilized `React Hook Form` and `Zod` for form validation and submission handling.
    - Used a currency list sorted alphabetically for easier currency selection selection.
    - Organized currency list alphabetically for better usability.
    - Ensured consistency in styles and UI components across the page.
    
    This commit enhances the Accounts page's user interface and functionality, providing a more seamless experience for users when managing their accounts.

--- client/src/components/containers/AddAndSort.jsx
+++ client/src/components/containers/AddAndSort.jsx
@@ -7,58 +7,86 @@ import {
 	Input,
 	Select,
 	SelectItem,
+	useDisclosure,
 } from "@nextui-org/react";
 import { SearchIcon } from "../ui/SearchIcon";
+import AccountForm from "../forms/AccountForm";
+import useAxios from "../../lib/useAxios";
+import { useMutation, useQueryClient } from "react-query";
 
 const AddAndSort = ({ sortBy, search }) => {
 	const [searchQuery, setSearchQuery] = useState("");
 	const [sortValue, setSortValue] = useState("a-z");
 
-	return (
-		<Card className="min-h-[600px] flex flex-col items-center">
-			<CardHeader className="text-2xl justify-center font-bold my-8">
-				Accounts
-			</CardHeader>
-			<CardBody className="flex flex-col gap-16 px-8 items-center max-w-xs">
-				<Button className="w-full bg-default-100 hover:bg-default-200 font-bold text-sm text-text-darkest">
-					Add an Account
-				</Button>
+	const axios = useAxios();
+	const { mutate: saveAccount } = useMutation(
+		(accountData) =>
+			axios.post("http://localhost:8000/api/v1/accounts/", accountData),
+		{
+			onSuccess: () => {
+				queryClient.invalidateQueries("accounts");
+				onOpenChange();
+			},
+		}
+	);
+	const queryClient = useQueryClient();
+	const { isOpen, onOpenChange, onOpen } = useDisclosure();
 
-				<Input
-					size="lg"
-					placeholder="Search..."
-					startContent={<SearchIcon size={18} />}
-					type="search"
-					value={searchQuery}
-					onValueChange={setSearchQuery}
-					onChange={(e) => search(e.target.value)}
-				/>
-				<Select
-					size="lg"
-					label="Sort by"
-					labelPlacement="outside"
-					defaultSelectedKeys={["a-z"]}
-					value={sortValue}
-					onChange={(e) => sortBy(e.target.value)}
-					onValueChange={setSortValue}
-					disallowEmptySelection>
-					<SelectItem key={"a-z"} value={"a-z"} defaultValue>
-						A-Z
-					</SelectItem>
-					<SelectItem key={"z-a"} value={"z-a"}>
-						Z-A
-					</SelectItem>
-					<SelectItem
-						key={"balance-highest"}
-						value={"balance-highest"}>
-						Balance - Highest First
-					</SelectItem>
-					<SelectItem key={"balance-lowest"} value={"balance-lowest"}>
-						Balance - Lowest First
-					</SelectItem>
-				</Select>
-			</CardBody>
-		</Card>
+	return (
+		<>
+			<Card className="min-h-[600px] flex flex-col items-center">
+				<CardHeader className="text-2xl justify-center font-bold my-8">
+					Accounts
+				</CardHeader>
+				<CardBody className="flex flex-col gap-16 px-8 items-center max-w-xs">
+					<Button
+						className="w-full bg-default-100 hover:bg-default-200 font-bold text-sm text-text-darkest"
+						onPress={onOpen}>
+						Add an Account
+					</Button>
+					<Input
+						size="lg"
+						placeholder="Search..."
+						startContent={<SearchIcon size={18} />}
+						type="search"
+						value={searchQuery}
+						onValueChange={setSearchQuery}
+						onChange={(e) => search(e.target.value)}
+					/>
+					<Select
+						size="lg"
+						label="Sort by"
+						labelPlacement="outside"
+						defaultSelectedKeys={["a-z"]}
+						value={sortValue}
+						onChange={(e) => sortBy(e.target.value)}
+						onValueChange={setSortValue}
+						disallowEmptySelection>
+						<SelectItem key={"a-z"} value={"a-z"} defaultValue>
+							A-Z
+						</SelectItem>
+						<SelectItem key={"z-a"} value={"z-a"}>
+							Z-A
+						</SelectItem>
+						<SelectItem
+							key={"balance-highest"}
+							value={"balance-highest"}>
+							Balance - Highest First
+						</SelectItem>
+						<SelectItem
+							key={"balance-lowest"}
+							value={"balance-lowest"}>
+							Balance - Lowest First
+						</SelectItem>
+					</Select>
+				</CardBody>
+			</Card>
+			<AccountForm
+				isOpen={isOpen}
+				onOpenChange={onOpenChange}
+				onSubmit={saveAccount}
+			/>
+		</>
 	);
 };
 
---
+++ client/src/components/forms/AccountForm.jsx
@@ -0,0 +1,243 @@
+import {
+	Modal,
+	ModalContent,
+	ModalHeader,
+	ModalBody,
+	ModalFooter,
+	Button,
+	Input,
+	Select,
+	SelectItem,
+} from "@nextui-org/react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, Controller } from "react-hook-form";
+import { jwtDecode } from "jwt-decode";
+import currencies from "../../lib/supportedCurrencies";
+
+const AccountForm = ({ isOpen, onOpenChange, onSubmit }) => {
+	const owner = jwtDecode(localStorage.getItem("access_token")).user_id;
+	const schema = z.object({
+		name: z
+			.string()
+			.min(3, { message: "Name must be at least 3 characters" }),
+		color: z.string().regex(/^#(?:[0-9a-fA-F]{3}){1,2}$/, {
+			message: "Please select a valid hex color code",
+		}),
+		owner: z.number().gt(0, { message: "Please provide account owner" }),
+		type: z.string().min(2, { message: "Please select an account type" }),
+		balance: z.string().transform((str) => parseInt(str)),
+		currency: z
+			.string()
+			.min(3, { message: "Please select account currency" }),
+	});
+	const { control, register, handleSubmit } = useForm({
+		defaultValues: {
+			name: "",
+			color: "#ffffff",
+			owner,
+			balance: 0,
+			currency: "TND",
+		},
+		resolver: zodResolver(schema),
+	});
+	return (
+		<>
+			<Modal
+				isOpen={isOpen}
+				onOpenChange={onOpenChange}
+				isDismissable={false}
+				className="min-h-[55vh]">
+				<ModalContent>
+					<ModalHeader className="flex flex-col gap-1">
+						<p className="pb-2 border-b">Add a new account</p>
+					</ModalHeader>
+					<ModalBody>
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<div className="grid grid-flow-col gap-4 mb-8">
+								<div className="col-span-6">
+									<Controller
+										name="name"
+										control={control}
+										render={({ field, fieldState }) => (
+											<Input
+												{...field}
+												{...register("name")}
+												isRequired
+												isInvalid={fieldState.invalid}
+												color={
+													fieldState.invalid
+														? "error"
+														: ""
+												}
+												errorMessage={
+													fieldState.error?.message
+												}
+												autoFocus
+												label="Account name"
+												variant="bordered"
+											/>
+										)}
+									/>
+								</div>
+								<div>
+									<Controller
+										name="color"
+										control={control}
+										render={({ field, fieldState }) => (
+											<Input
+												{...field}
+												{...register("color")}
+												isRequired
+												isInvalid={fieldState.invalid}
+												color={
+													fieldState.invalid
+														? "error"
+														: ""
+												}
+												errorMessage={
+													fieldState.error?.message
+												}
+												label="Color"
+												variant="bordered"
+												type="color"
+												className="max-w-16"
+											/>
+										)}
+									/>
+								</div>
+							</div>
+							<Controller
+								name="type"
+								control={control}
+								render={({ field, fieldState }) => (
+									<Select
+										className="mb-8"
+										{...field}
+										{...register("type")}
+										isRequired
+										isInvalid={fieldState.invalid}
+										color={
+											fieldState.invalid ? "error" : ""
+										}
+										errorMessage={fieldState.error?.message}
+										label="Account type"
+										disallowEmptySelection>
+										<SelectItem key={"GEN"} value={"GEN"}>
+											General
+										</SelectItem>
+										<SelectItem key={"CASH"} value={"CASH"}>
+											Cash
+										</SelectItem>
+										<SelectItem key={"CA"} value={"CA"}>
+											Current Account
+										</SelectItem>
+										<SelectItem key={"SAV"} value={"SAV"}>
+											Saving Account
+										</SelectItem>
+										<SelectItem key={"INV"} value={"INV"}>
+											Investment Account
+										</SelectItem>
+									</Select>
+								)}
+							/>
+							<div className="grid grid-flow-col gap-4 mb-8">
+								<div className="col-span-2">
+									<Controller
+										name="balance"
+										control={control}
+										render={({ field, fieldState }) => (
+											<Input
+												{...field}
+												{...register("balance")}
+												isRequired
+												isInvalid={fieldState.invalid}
+												color={
+													fieldState.invalid
+														? "error"
+														: ""
+												}
+												errorMessage={
+													fieldState.error?.message
+												}
+												onChange={(e) => {
+													const value = parseFloat(
+														e.target.value
+													);
+													field.onChange(value);
+												}}
+												label="Initial balance"
+												variant="bordered"
+												type="number"
+											/>
+										)}
+									/>
+								</div>
+								<div className="col-span-10">
+									<Controller
+										name="currency"
+										control={control}
+										render={({ field, fieldState }) => (
+											<Select
+												{...field}
+												{...register("currency")}
+												isRequired
+												isInvalid={fieldState.invalid}
+												color={
+													fieldState.invalid
+														? "error"
+														: ""
+												}
+												errorMessage={
+													fieldState.error?.message
+												}
+												items={Object.keys(currencies)}
+												label="Account currency"
+												selectionMode="single">
+												{Object.entries(currencies).map(
+													(kvp) => (
+														<SelectItem
+															key={kvp[0]}
+															value={kvp[1]}>
+															{kvp[1]}
+														</SelectItem>
+													)
+												)}
+											</Select>
+										)}
+									/>
+								</div>
+							</div>
+
+							<Controller
+								name="owner"
+								control={control}
+								render={({ field, fieldState }) => (
+									<input
+										{...field}
+										{...register("owner")}
+										value={owner}
+										type="hidden"
+									/>
+								)}
+							/>
+							<div className="flex justify-end gap-5">
+								<Button
+									color="danger"
+									variant="flat"
+									onPress={onOpenChange}>
+									Cancel
+								</Button>
+								<Button color="primary" type="submit">
+									Save
+								</Button>
+							</div>
+						</form>
+					</ModalBody>
+				</ModalContent>
+			</Modal>
+		</>
+	);
+};
+
+export default AccountForm;
